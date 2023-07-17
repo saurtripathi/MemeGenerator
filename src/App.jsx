342eda3727831597da46ae6b5e541ddc2fe8a4ac
Saurabh Tripathi <saurabh.umesh.tripathi@gmail.com>
2023-07-17 19:11:43 +0530

    added some code

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,15 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './Header'
 import Meme from './Meme'
-// import {getMemesData} from './Data.js'
 
 
-async function getMemesData() {
-  const response = await fetch('https://api.imgflip.com/get_memes')
-  const data = await response.json()   
-  return data;
-}
+
 
 function App() {
 
-
-  console.log(getMemesData())
   return (
     <>
-     <Header />
+      <Header />
       <Meme />
     </>
   )
--- src/Meme.jsx
+++ src/Meme.jsx
@@ -1,34 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+import memeimg from '/memeimg.jpg'
 import './App.css'
 import Header from './Header'
-import Data from './Data.js'
+
 
 function Meme() {
-    const memesArray = Data.data.memes
 
-    const memesArrayIndex = Math.floor(Math.random() * memesArray.length)
+    const [meme, setMeme] = useState(
+                                        {   topText:"",
+                                            bottomText:"",
+                                            randomImage:memeimg
+                                        }
+                                    )
+    // console.log(defaultMemeImg)
 
-    let [imageUrl, setUrl] = useState(memesArray[memesArrayIndex].url)
-    console.log(imageUrl)
+    const [allMemes,setAllMemes] = useState([])
 
-    function handleClick() {
-        setUrl(prevUrlState => imageUrl = prevUrlState)
-        console.log(imageUrl)
+    useEffect(() => {
+        fetch(`https://api.imgflip.com/get_memes`)
+        .then(res => res.json())
+        .then(data => setAllMemes(data.data.memes))
+    },[])
+
+    function handleChange(event){
+        
+        const{name,value} = event.target
+        console.log(name,value)
+        setMeme(prevMeme => {
+            return {
+                ...prevMeme,
+                [name]:value
+            }
+        })
+    }
+    function getMemeImage() {
+        console.log('clicked')
+        const memesArray = allMemes
+       
+        const memesArrayIndex = Math.floor(Math.random() * memesArray.length)
+        // console.log(allMemes[memesArrayIndex].url)
+        setMeme(prevMeme => {
+            return { 
+                ...prevMeme,
+                randomImage:allMemes[memesArrayIndex].url
+            }
+        })
     }
     return (
         <>
-            <form>
+            
                 <div className='form--inp-group'>
-                    <input className='form--input-1' type='text' placeholder='Shut up' />
-                    <input className='form--input-2' type='text' placeholder='and take my money' />
+                    <input 
+                    name="topText" 
+                    value={meme.topText} 
+                    className='form--input-1' 
+                    type='text' 
+                    placeholder='Shut up' 
+                    onChange={handleChange}
+                    />
+                    <input 
+                    name="bottomText" 
+                    value={meme.bottomText} 
+                    className='form--input-2' 
+                    type='text' 
+                    placeholder='and take my money' 
+                    onChange={handleChange}
+                    />
                 </div>
-                <button onClick={handleClick} className='form--btn'>Get a new meme image  🖼</button>
+                <button 
+                    className='form--btn'
+                    onClick={getMemeImage}
+                >Get a new meme image  🖼     
+                </button>
                 <div className='meme--image--div'>
-                    <img className='meme--image' src={imageUrl} />
+                    <img className='meme--image' src={meme.randomImage} />
+                    <p className='meme--top'>{meme.topText}</p>
+                    <p className='meme--bottom'>{meme.bottomText}</p>
                 </div>
-            </form>
+            
         </>
     )
 }
