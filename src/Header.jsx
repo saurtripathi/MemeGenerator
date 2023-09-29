1ff29d86c04dba67a24ad0d333378e806f320308
saurtripathi <127930624+saurtripathi@users.noreply.github.com>
2023-09-29 12:05:43 +0530

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -8,7 +8,6 @@ function Header() {
      <header>
         <img src='Troll Face.png'alt='Troll Face'/>
         <h2>Meme Generator</h2>
-        <p>React Course - Project 3</p>
      </header>
     </>
   )
